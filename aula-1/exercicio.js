4cc45c2ca14a405017d57759efb0cad2cd60590e
a-luz <arthurg.luz@gmail.com>
2023-03-01 13:31:56 -0300

    aula 1

---
+++ aula-1/exercicio.js
@@ -0,0 +1,33 @@
+//Criar a funcao getCharFrequencies. Essa funcao deve receber uma string e devolver
+//a frequencia de cada caracter da string.
+
+var str1 = 'world';
+var str2 = 'rekqodlw';
+
+var str3 = 'codewars';
+var str4 = 'cedewaraaossoqqyt';
+
+var str5 = 'steak';
+var str6 = 'katas';
+
+var str7 = '';
+var str8 = '';
+
+function getCharFrequencies(s){
+
+}
+
+
+//Criar a funcao contem onde serao passadas duas strings (s1 e s2).
+//contem? devera verificar se os caracteres (com quantidade) de s1 estao contidos
+//em s2. Retorna true ou false.
+
+function contem(s1, s2){
+}
+
+
+console.log(contem(str1, str2)); // true
+console.log(contem(str3, str4)); // true
+console.log(contem(str5, str6)); // false
+console.log(contem(str7, str8)); // false
+console.log(contem(str8, str7)); // false
